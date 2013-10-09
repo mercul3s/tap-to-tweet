14cdb0e910df8eba7bdcc41265cf8e077c563787
Mercedes Coyle <mcoyle@realgravity.com>
2013-10-08 22:48:57 -0700

    consolidating some code and moving files around, merging python firmata/breakfastserial code

---
+++ morse_arduino/morse_arduino.ino
@@ -0,0 +1,43 @@
+// Turn on LED while button is pressed
+
+const int LED = 13;       //the pin for the LED
+const int BUTTON = 7;     //the input pin where
+                          //pushbutton is connected
+int ledState = LOW;     // the current state of the output pin
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW; // the previous reading from the input pin
+
+// the following variables are long's because the time, measured in milliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;   // the debounce time; increase if the output flickers
+
+void setup() {
+  pinMode (LED, OUTPUT);
+  pinMode (BUTTON, INPUT);
+  digitalWrite(LED, ledState);
+  Serial.begin(9600);
+}
+
+void loop() {
+ int reading = digitalRead(BUTTON); // read the input value of the button
+  
+  // check to see if we pressed the button (ie, the state has changed to HIGH)
+  // if so, 
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    
+    if (reading != buttonState) {
+      buttonState = reading;   
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  digitalWrite(LED, ledState);
+  lastButtonState = reading;
+}
