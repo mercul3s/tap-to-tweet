9fcecf8add1fc5c4c34bc1e32f26d2fbb10ccf7f
Mercedes Coyle <mcoyle@realgravity.com>
2013-10-12 20:23:28 -0700

    adding LCD and tested with hello world

--- morse_arduino/morse_arduino.ino
+++ morse_arduino/morse_arduino.ino
@@ -1,5 +1,6 @@
-// Turn on LED while button is pressed
+#include <LiquidCrystal.h>
 
+// Turn on LED while button is pressed
 const int LED = 13;       //the pin for the LED
 const int BUTTON = 7;     //the input pin where
                           //pushbutton is connected
@@ -11,11 +12,16 @@ int lastButtonState = LOW; // the previous reading from the input pin
 // will quickly become a bigger number than can be stored in an int.
 long lastDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 50;   // the debounce time; increase if the output flickers
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   pinMode (LED, OUTPUT);
   pinMode (BUTTON, INPUT);
   digitalWrite(LED, ledState);
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
   Serial.begin(9600);
 }
 
