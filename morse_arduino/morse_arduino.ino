4556ac140de8ac78ceb82c720a9c7148c2ff2e9e
Mercedes Coyle <mcoyle@realgravity.com>
2013-10-12 22:17:30 -0700

    getting a little closer with button states and lcd output

--- morse_arduino/morse_arduino.ino
+++ morse_arduino/morse_arduino.ino
@@ -1,17 +1,17 @@
 #include <LiquidCrystal.h>
 
-// Turn on LED while button is pressed
-const int LED = 13;       //the pin for the LED
-const int BUTTON = 7;     //the input pin where
-                          //pushbutton is connected
-int ledState = LOW;     // the current state of the output pin
+const int LED = 13;         
+const int BUTTON = 7;      
+int ledState = LOW;         // the default state of the output pin
 int buttonState;            // the current reading from the input pin
-int lastButtonState = LOW; // the previous reading from the input pin
+int lastButtonState = LOW;  // the previous reading from the input pin, defaults to LOW
 
-// the following variables are long's because the time, measured in milliseconds,
+// the following variables are long because the time, measured in milliseconds,
 // will quickly become a bigger number than can be stored in an int.
+
 long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 50;   // the debounce time; increase if the output flickers
+long debounceDelay = 100;   // the debounce time; increase if the output flickers
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
@@ -20,30 +20,33 @@ void setup() {
   digitalWrite(LED, ledState);
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
-  Serial.begin(9600);
+  Serial.begin(9600);       // Console output for debugging and sending
+                            // data to wrapper script
 }
 
 void loop() {
- int reading = digitalRead(BUTTON); // read the input value of the button
+ int current_state = digitalRead(BUTTON); // read the input value of the button
   
   // check to see if we pressed the button (ie, the state has changed to HIGH)
-  // if so, 
-  if (reading != lastButtonState) {
+  // if so, turn on the LED and print a message to the LCD
+  if (current_state != lastButtonState) {
     lastDebounceTime = millis();
   }
   
   if ((millis() - lastDebounceTime) > debounceDelay) {
     
-    if (reading != buttonState) {
-      buttonState = reading;   
+    if (current_state != buttonState) {
+      buttonState = current_state;   
       if (buttonState == HIGH) {
         ledState = !ledState;
+        digitalWrite(LED, buttonState); 
+        lcd.print("hello, world!");
+        delay(1000);
+        lcd.clear();
       }
     }
   }
 
   digitalWrite(LED, ledState);
-  lastButtonState = reading;
+  lastButtonState = current_state;
 }
